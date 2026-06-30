c195c56aebfa6c354b2133d7a5a9ebc2cd970546
riagnisapuri-cmyk <riagnisapuri@gmail.com>
2026-06-30 15:05:36 +0800

    Update koneksi.example.php

--- config/koneksi.example.php
+++ config/koneksi.example.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "sibansos";
+$user = "username_database";
+$pass = "password_database";
+$db   = "nama_database";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
